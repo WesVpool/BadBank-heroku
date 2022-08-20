86eb323fb5add3c66403ea0133c3372fdd2f1313
WesVpool <westonvanderpool@gmail.com>
2022-08-19 19:41:53 -0500

    fixed local and deploy mongo connection

--- dal.js
+++ dal.js
@@ -1,8 +1,11 @@
-const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017';
+const dotenv = require("dotenv");
+dotenv.config({path: "./config.env"});
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require("mongodb");
+const uri = process.env.MONGODB_URI;
 let db = null;
 
-MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
+MongoClient.connect(uri, {useUnifiedTopology: true}, function(err, client) {
     console.log('Connected successfully to db server!')
 
     // connect to myproject database
