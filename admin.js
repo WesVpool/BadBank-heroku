2ffbc9232f6d01ad6bf9016f5d0db2ea125cdd40
WesVpool <westonvanderpool@gmail.com>
2022-08-22 19:51:20 -0500

    Config vars admin

--- admin.js
+++ admin.js
@@ -1,6 +1,7 @@
 const admin = require('firebase-admin');
-
-const serviceAccount = require("./badbankAuth.json");
+const dotenv = require("dotenv");
+dotenv.config({path: "./config.env"});
+const serviceAccount = process.env.GOOGLE_CREDS;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
