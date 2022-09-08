3057d8e38ba55b124cc25e61cdca56cc01c53d80
WesVpool <westonvanderpool@gmail.com>
2022-09-08 07:54:01 -0500

    removed console.log

--- public/create.js
+++ public/create.js
@@ -62,7 +62,6 @@ function CreateAccount(){
   }
 
   function handleCreate(){
-    console.log(name,email,password);
     if (!validate(name,     'name'))     return;
     if (!validate(email,    'email'))    return;
     if (!validate(password, 'password')) return;
